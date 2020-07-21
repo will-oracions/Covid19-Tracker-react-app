b82b687a99f465efc04d3b5604da23338d6eae06
Oracions <Oracions.dev@gmail.com>
2020-07-21 11:11:35 +0100

    initialisation du projet sous windows

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+
+    render() {
+        return <h1>Portez ce whisky au vieux juge blond qui fume !</h1>
+    }
+}
+
+export default App
